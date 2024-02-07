6c8c1cdb5ec21c61f307b6b7c861e232dad382c6
Abdülfettah Adwani <fettahaud@gmail.com>
2024-02-07 08:45:39 +0300

    Change path

--- src/Car.jsx
+++ src/Car.jsx
@@ -2,7 +2,7 @@ import React, { useRef } from "react";
 import { useGLTF } from "@react-three/drei";
 
 export function Car(props) {
-  const { nodes, materials } = useGLTF("./Car_1_V001.glb");
+  const { nodes, materials } = useGLTF("/Car_1_V001.glb");
   return (
     <group {...props} dispose={null} scale={0.02}>
       <group rotation={[Math.PI / 2, 0, 0]}>
@@ -35,4 +35,4 @@ export function Car(props) {
   );
 }
 
-useGLTF.preload("./Car_1_V001.glb");
+useGLTF.preload("/Car_1_V001.glb");
