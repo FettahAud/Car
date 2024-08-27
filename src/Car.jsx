ddee2220ae2b0cdaffc101dc09b560cc2edbef44
Abdülfettah Adwani <fettahaud@gmail.com>
2024-08-27 20:26:14 +0300

    backup

--- src/Car.jsx
+++ src/Car.jsx
@@ -2,37 +2,51 @@ import React, { useRef } from "react";
 import { useGLTF } from "@react-three/drei";
 
 export function Car(props) {
-  const { nodes, materials } = useGLTF("/Car_1_V001.glb");
+  const group = useRef();
+  const { nodes, materials } = useGLTF("/Car_1_V003.glb");
+  // group.current.children[0].children[0].children[1].material.depthWrite = true;
   return (
-    <group {...props} dispose={null} scale={0.02}>
-      <group rotation={[Math.PI / 2, 0, 0]}>
-        <mesh
-          castShadow
-          receiveShadow
-          geometry={nodes.v1_0_1.geometry}
-          material={materials.Interior_lambert43}
-        />
-        <mesh
-          castShadow
-          receiveShadow
-          geometry={nodes.v1_0_2.geometry}
-          material={materials.Engine_Parts_lambert41}
-        />
-        <mesh
-          castShadow
-          receiveShadow
-          geometry={nodes.v1_0_3.geometry}
-          material={materials.Exterior_lambert44}
-        />
-        <mesh
-          castShadow
-          receiveShadow
-          geometry={nodes.v1_0_4.geometry}
-          material={materials.Hood_Lid_lambert42}
-        />
+    <group ref={group} {...props} dispose={null}>
+      <group name="Scene">
+        <group name="Vehicle_1_Low" rotation={[Math.PI / 2, 0, 0]} scale={0.01}>
+          <mesh
+            name="Engine_Parts_Low"
+            castShadow
+            receiveShadow
+            geometry={nodes.Engine_Parts_Low.geometry}
+            material={materials.Engine_Parts_lambert41}
+          />
+          <mesh
+            name="Exterior_Low"
+            castShadow
+            receiveShadow
+            geometry={nodes.Exterior_Low.geometry}
+            // material={materials.Exterior_lambert44}
+          >
+            <meshStandardMaterial
+              name="Exterior_lambert44"
+              {...materials.Exterior_lambert44}
+              depthWrite={true}
+            />
+          </mesh>
+          <mesh
+            name="Hood_Lid_Low"
+            castShadow
+            receiveShadow
+            geometry={nodes.Hood_Lid_Low.geometry}
+            material={materials.Hood_Lid_lambert42}
+          />
+          <mesh
+            name="Interior_Low"
+            castShadow
+            receiveShadow
+            geometry={nodes.Interior_Low.geometry}
+            material={materials.Interior_lambert43}
+          />
+        </group>
       </group>
     </group>
   );
 }
 
-useGLTF.preload("/Car_1_V001.glb");
+useGLTF.preload("/Car_1_V003.glb");
--- src/Experience.jsx
+++ src/Experience.jsx
@@ -1,8 +1,14 @@
-import { Environment, OrbitControls } from "@react-three/drei";
+import { Environment, OrbitControls, useFBX } from "@react-three/drei";
 import { Perf } from "r3f-perf";
 import { Car } from "./Car";
+import { useLoader } from "@react-three/fiber";
+import { FBXLoader } from "three/examples/jsm/loaders/FBXLoader";
 
 export default function Experience() {
+  // import fbx model
+  // const model = useLoader(FBXLoader, "/Car_1_V003.fbx");
+  // console.log(model);
+  // const model = useGLTF("/Car_1_V003.glb");
   return (
     <>
       <Perf position="top-left" />
@@ -11,9 +17,10 @@ export default function Experience() {
 
       {/* <directionalLight castShadow position={[1, 5, 3]} intensity={1.5} />
       <ambientLight intensity={4} /> */}
-      <Environment preset="city" />
+      <Environment preset="forest" />
 
-      <Car />
+      <Car position={[0, 0, 0]} />
+      {/* <primitive object={model} scale={0.01} /> */}
     </>
   );
 }
